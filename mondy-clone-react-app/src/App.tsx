b9106d236b8efd897aded3171d7a297495e747bc
kimyongyeon <toyongyeon@gmail.com>
2020-08-16 01:24:40 +0900

    eject edit

--- mondy-clone-react-app/src/App.tsx
+++ mondy-clone-react-app/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Counter from './pages/counter';
 
 function App() {
   return (
     <div className="App">
       <h1>hello monday clone react app</h1>
+      <Counter name='hello' age={12} />
     </div>
   );
 }
